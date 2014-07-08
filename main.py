1d920588b0f2a9340d35e89d62c741c3600f3bbc
Alexander Bangert <ab@xentary.de>
2014-07-08 17:28:45 +0200

    do update

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import socket
 
+from System import System
+
+
 hostname = socket.gethostname()
-ostype = "rpm"
+
+def receive_command(command):
+	if (command == u"system-update"):
+		do_system_update()
+
+def do_system_update():
+	s = System()
+	s.do_update()
+
+
+receive_command(u"system-update")
